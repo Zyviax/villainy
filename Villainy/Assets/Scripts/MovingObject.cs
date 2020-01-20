c3f300ca772a47208ec427ee2efa0c6b777d88cc
Damian Soo <sooda336@student.otago.ac.nz>
2020-01-20 16:24:19 +1300

    Created play/pause and fast forward controls

---
+++ Villainy/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayPause.playing)
+        {
+            // Move the object forward along its x axis 1 unit/second
+            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);            
+        }
+        
+    }
+}
--- Villainy/Assets/Scripts/PlayPause.cs
+++ Villainy/Assets/Scripts/PlayPause.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayPause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool playing;   
+    public Button myBtn;   
+    public Sprite Play;
+    public Sprite Pause;
+    public KeyCode playControl;
+
+    public static bool fastForward;
+    public Button ffBtn;
+    public Sprite FastFowardOff;
+    public Sprite FastForwardOn;
+    public KeyCode ffControl;
+
+    public void PlayClick()
     {
-        
+        ChangePlayState();
     }
 
-    // Update is called once per frame
+    public void ChangePlayState()
+    {
+        playing = !playing;
+        if(playing)
+        {
+            myBtn.image.sprite = Pause;
+        }
+        else
+        {
+            myBtn.image.sprite = Play;
+        }
+    }
+
+    public void FfClick()
+    {
+        ChangeFfState();
+    }
+
+    public void ChangeFfState()
+    {
+        fastForward = !fastForward;
+        if (fastForward)
+        {
+            ffBtn.image.sprite = FastForwardOn;
+            Time.timeScale = 2;
+        }
+        else
+        {
+            ffBtn.image.sprite = FastFowardOff;
+            Time.timeScale = 1;
+        }
+    }
+
     void Update()
     {
-        
+        if(Input.GetKeyUp(playControl))
+        {
+            ChangePlayState();
+        }
+
+        if(Input.GetKeyUp(ffControl))
+        {
+            ChangeFfState();
+        }
     }
 }
