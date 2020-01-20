3800507999ad6ad892981b75b15599a37696b264
Spring Han <hansp965@student.otago.ac.nz>
2020-01-20 15:43:51 +1300

    Added node system

---
+++ Villainy/Assets/Scripts/BasicNodePath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicNodePath : MonoBehaviour
+{
+    public List<Transform> nodePath;
+    public GameObject node;
+
+    public Transform startNode;
+    public Transform endNode;
+
+    public void CreateNewNode()
+    {
+        Transform nextNode = Instantiate(node, endNode.position, Quaternion.identity).transform;
+        nextNode.SetParent(this.transform);
+        nextNode.name = "Node" + nodePath.Count;
+
+        endNode.GetComponent<Node>().nextNode = nextNode.transform;
+
+        nodePath.Add(nextNode.transform);
+        endNode = nextNode;
+    }
+}
