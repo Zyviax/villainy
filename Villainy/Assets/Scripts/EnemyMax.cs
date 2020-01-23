b6cb809284861cecdce7a09efbbbb4ae125ddc68
Max Edwards <edwma571@student.otago.ac.nz>
2020-01-23 14:41:54 +1300

    Basic prototype of Tower AI and AOE

---
+++ Villainy/Assets/Scripts/EnemyMax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMax : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        /*Projectile projectile = other.GetComponent<Projectile>();
+        if(projectile != null)
+        {
+            Destroy(other.gameObject);
+            health -= 1;
+            if(health==0)
+            {
+                Destroy(this.gameObject);
+            }
+        }*/
+    }
+}
---
+++ Villainy/Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+    public float hitSize;
+    public Transform aoePrefab;
+    private bool aoeMade = false;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyMax enemy = other.GetComponent<EnemyMax>();
+        if(enemy!=null)
+        {
+            if(hitSize>0 && !aoeMade)
+            {
+                Transform aoe = Instantiate(aoePrefab);
+                aoe.position = transform.position;
+                aoeMade = true;
+                CircleCollider2D aoeCircle = aoe.GetComponent<CircleCollider2D>();
+                aoeCircle.radius = hitSize;
+                Destroy(this.gameObject);
+                return;
+            }
+
+            enemy.health--;
+            Transform enemyTransform = other.GetComponent<Transform>();
+            enemyTransform.localScale *= .6f;
+            if (enemy.health==0)
+            {
+                Destroy(other.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
