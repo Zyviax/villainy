2c13ed2ab83078d63ef95272a02a70afd4bcd4db
Max Edwards <edwma571@student.otago.ac.nz>
2020-01-27 16:47:07 +1300

    Started setting up spells

---
+++ Villainy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+    Spells:
+    0 = no spell selected
+    1 = Stun Spell
+    2 = Heal Spell
+    3 = Speed Spell
+         */
+    static int currentSpell = 0;
+    static Transform aimPrefab;
+
+    static void useSpell(int spell)
+    {
+        currentSpell = spell;
+    }
+
+    private void Update()
+    {
+        if(currentSpell!=0)
+        {
+
+        }
+    }
+}
