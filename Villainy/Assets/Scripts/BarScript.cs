7a0a598745849fed33ecb3c9d829fb829ef0b602
Spring Han <hansp965@student.otago.ac.nz>
2020-02-10 13:29:03 +1300

    Some bar stuff

---
+++ Villainy/Assets/Scripts/BarScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    public Image image;
+    public Image secondImage;
+
+    public float imageScale;
+    public float secondImageScale;
+
+    public float firstVariable = 100;
+    public float secondVariable = 100;
+
+    public float thirdVariable = 30;
+
+    private void Start()
+    {
+        //image.transform.localScale
+        //secondImage.transform.localScale = new Vector3(secondImageScale, ;
+    }
+
+    void Update()
+    {
+        image.fillAmount = firstVariable / secondVariable;
+    }
+
+    private void OnMouseEnter()
+    {
+        
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+}
