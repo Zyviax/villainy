bd078d239301ee9e52bf20635e3ca976516b5a90
Garth Wales <walga757@student.otago.ac.nz>
2020-02-14 10:57:58 +1300

    Implement persistent audio and audio picking script. Add a blue lightning bolt. Changed the music system for all levels.

---
+++ Villainy/Assets/Scripts/MusicPicker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPicker : MonoBehaviour
+{
+    public AudioClip NewMusic;
+
+    void Awake()
+        {
+        AudioSource go = GameObject.FindGameObjectWithTag("MusicManager").GetComponent<AudioSource>();
+        go.clip = NewMusic; //Replaces the old audio with the new one set in the inspector.
+        go.Play(); //Plays the audio.
+    }
+}
--- Villainy/Assets/Scripts/Stats.cs
+++ Villainy/Assets/Scripts/Stats.cs
@@ -16,21 +16,21 @@ public class Stats : MonoBehaviour
         damage.text = "<b>Damage: </b>" + GameyManager.damageTaken;
         spells.text = "<b>Spells: </b>" + GameyManager.spellsCast;
 
-        dead.text = "<b>Units Dead</b>\n" +
+        dead.text = "<b>Dead Units</b>\n" +
             "\n Imp: " + GameyManager.enemiesDead[0] +
             "\n Priest: " + GameyManager.enemiesDead[1] +
             "\n Turtle: " + GameyManager.enemiesDead[2] +
             "\n Valkyrie: " + GameyManager.enemiesDead[3] +
             "\n Work Master: " + GameyManager.enemiesDead[4];
 
-        spawned.text = "<b>Units Spawned</b>\n" +
+        spawned.text = "<b>Spawned Units</b>\n" +
             "\n Imp: " + GameyManager.enemiesSpawned[0] +
             "\n Priest: " + GameyManager.enemiesSpawned[1] +
             "\n Turtle: " + GameyManager.enemiesSpawned[2] +
             "\n Valkyrie: " + GameyManager.enemiesSpawned[3] +
             "\n Work Master: " + GameyManager.enemiesSpawned[4];
 
-        end.text = "<b>Units Reached End</b>\n" +
+        end.text = "<b>Successful Units</b>\n" +
             "\n Imp: " + GameyManager.enemiesReachedEnd[0] +
             "\n Priest: " + GameyManager.enemiesReachedEnd[1] +
             "\n Turtle: " + GameyManager.enemiesReachedEnd[2] +
